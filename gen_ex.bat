fc32066c85ea14b1bcd7c7bbfb06763af874609b
Berako00 <126508321+Berako00@users.noreply.github.com>
2025-03-17 15:25:06 +0100

    Update gen_ex.bat

--- gen_ex.bat
+++ gen_ex.bat
@@ -1,7 +1,17 @@
 @echo off
 pip install pyinstaller
+pyinstaller --onefile --windowed complex_main.py
+
+:: Wait for PyInstaller to finish
+timeout /t 5 /nobreak >nul
+
+:: Set paths
+set "SOURCE=%CD%\dist\complex_main.exe"
 set "DESKTOP=%USERPROFILE%\Desktop"
-pyinstaller --onefile --windowed --distpath "%DESKTOP%" complex_main.py
+set "SHORTCUT=%DESKTOP%\complex_main.lnk"
 
+:: Create shortcut on the Desktop
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%SHORTCUT%'); $s.TargetPath='%SOURCE%'; $s.Save()"
 
+echo Shortcut created on Desktop!
 
